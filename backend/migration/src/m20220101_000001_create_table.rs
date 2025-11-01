7f7a90ac1e1151a8786a83a77acf11e96ec31ab9
Parth2684 <bhosle6006@gmail.com>
2025-11-01 15:29:00 +0530

    added sea-orm and did the first migration and entity generation

--- backend/migration/src/m20220101_000001_create_table.rs
+++ backend/migration/src/m20220101_000001_create_table.rs
@@ -1,42 +1,77 @@
-use sea_orm_migration::{prelude::*, schema::*};
+use sea_orm_migration::{prelude::{extension::postgres::Type, *}};
 
 #[derive(DeriveMigrationName)]
 pub struct Migration;
 
-#[async_trait::async_trait]
-impl MigrationTrait for Migration {
+#[async_trait::async_trait]impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         // Replace the sample below with your own migration scripts
-        todo!();
+        manager
+            .create_type(
+                Type::create()
+                    .as_enum(QuotaType::Table)
+                    .values(["Free", "Bronze", "Silver", "Gold", "Platinum"])
+                    .to_owned()
+            )
+            .await?;
         manager
             .create_table(
                 Table::create()
-                    .table(Post::Table)
+                    .table(User::Table)
                     .if_not_exists()
-                    .col(pk_auto(Post::Id)
-                        
+                    .col(ColumnDef::new(User::Id)
+                        .uuid()
+                        .not_null()
+                        .unique_key()
+                        .primary_key()
                     )
-                    .col(string(Post::Title))
-                    .col(string(Post::Text))
+                    .col(ColumnDef::new(User::Gmail)
+                        .string()
+                        .not_null()
+                        .unique_key()
+                    )
+                    .col(ColumnDef::new(User::CreatedAt)
+                        .date_time()
+                        .not_null()
+                    )
+                    .col(ColumnDef::new(User::Name)
+                        .string()
+                        .not_null()
+                    )
+                    .col(ColumnDef::new(User::Image)
+                        .string()
+                    )
+                    .col(ColumnDef::new(User::QuotaType)
+                        .enumeration(QuotaType::Table, ["Free", "Bronze", "Silver", "Gold", "Platinum"])
+                        .not_null()
+                        .default("Free")
+                    )
                     .to_owned(),
             )
             .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
 
         manager
-            .drop_table(Table::drop().table(Post::Table).to_owned())
-            .await
+            .drop_table(Table::drop().table(User::Table).to_owned())
+            .await?;
+        manager
+            .drop_type(Type::drop().name(QuotaType::Table).to_owned()).await
     }
 }
 
 #[derive(DeriveIden)]
-enum Post {
+enum User {
     Table,
     Id,
-    Title,
-    Text,
+    Gmail,
+    CreatedAt,
+    Name,
+    QuotaType,
+    Image
+}
+#[derive(DeriveIden)]
+enum QuotaType {
+    Table
 }
--- backend/migration/src/user.rs
+++
@@ -1,7 +0,0 @@
-use sea_orm::prelude::*;
-
-#[derive(Debug, Clone, PartialEq, Eq, DeriveEntityModel, Default)]#[sea_orm(table_name = "users")]
-pub struct Model {
-    #[sea_orm(primary_key)]
-    id: 
-}   
