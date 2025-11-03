62b38e69eb936f19040e189ad642fdfab0a3c636
Parth2684 <bhosle6006@gmail.com>
2025-11-03 20:20:50 +0530

    added oauth a little

--- backend/api/src/export_envs.rs
+++ backend/api/src/export_envs.rs
@@ -2,6 +2,10 @@ use once_cell::sync::Lazy;
 
 pub struct Envs {
     pub database_url: String,
+    pub google_client_id: String,
+    pub google_client_secret: String,
+    pub google_client_redirect_url: String,
+    pub backend_url: String,
 }
 
 
@@ -9,5 +13,13 @@ pub static ENVS: Lazy<Envs> = Lazy::new(||{
     dotenvy::dotenv().ok();
     let database_url = dotenvy::var("DATABASE_URL")
         .expect("DATABASE_URL not found in the environment");
-    Envs { database_url }
+    let google_client_id = dotenvy::var("GOOGLE_CLIENT_ID")
+        .expect("Google Client Id for Oauth2 must be provided");
+    let google_client_secret = dotenvy::var("GOOGLE_CLIENT_SECRET")
+        .expect("Google Client Secret for Oauth2 must be provided");
+    let google_client_redirect_url = dotenvy::var("GOOGLE_CLIENT_REDIRECT_URL")
+        .expect("Goodle Client Redirect Url must be provided");
+    let backend_url = dotenvy::var("BACKEND_URL")
+        .expect("Backend Url must be provided");
+    Envs { database_url, google_client_id, google_client_secret, google_client_redirect_url, backend_url }
 });
--- backend/api/src/handlers/auth/login_with_google.rs
+++ backend/api/src/handlers/auth/login_with_google.rs
@@ -1,6 +1,43 @@
+use axum::{extract::Query, response::Redirect};
+use url::Url;
+use reqwest::Client;
 
+use crate::export_envs::ENVS;
 
-
-pub fn login_with_google() {
-    
+struct AuthRequest {
+    code: String,
+    state: String
 }
+
+
+pub fn google_auth_redirect() -> Redirect {
+    let client_id = &ENVS.google_client_id;
+    let redirect_url = &ENVS.google_client_redirect_url;
+    
+    let auth_url = Url::parse_with_params("https://accounts.google.com/o/oath2/v2/auth", &[
+        ("client_id", client_id.as_str()),
+        ("redirect_url", redirect_url.as_str()),
+        ("response_type", "code"),
+        ("scope", "openid email profile"),
+        ("acces_type", "offline"),
+        ("prompt", "consent")
+    ]);
+    match auth_url {
+        Ok(url) => Redirect::to(url.as_str()),
+        Err(err) => {
+            println!("{err:?}");
+            Redirect::to("/auth/google")
+        }
+    }
+}
+
+
+pub async fn google_auth_callback(Query(params): Query<AuthRequest>) {
+    let client = Client::new();
+    let token_url = String::from("https://oauth2.googleapis.com/token");
+    let client_id = &ENVS.google_client_id;
+    let client_secret = &ENVS.google_client_secret;
+    let redirect_url = &ENVS.google_client_redirect_url;
+    
+    
+} 
