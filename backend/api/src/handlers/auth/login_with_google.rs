15ae520f814ac1d5480759bfedbcdc9e6e5acfb4
Parth2684 <bhosle6006@gmail.com>
2025-11-04 00:42:13 +0530

    will continue tomorrow

--- backend/api/src/handlers/auth/login_with_google.rs
+++ backend/api/src/handlers/auth/login_with_google.rs
@@ -39,5 +39,59 @@ pub async fn google_auth_callback(Query(params): Query<AuthRequest>) {
     let client_secret = &ENVS.google_client_secret;
     let redirect_url = &ENVS.google_client_redirect_url;
     
+    let res = client.post(token_url)
+        .form(&[
+            ("code", params.code.as_str()),
+            ("client_id", client_id.as_str()),
+            ("client_secret", client_secret.as_str()),
+            ("redirect_uri", redirect_url.as_str()),
+            ("grant_type", "authorization_code")
+        ])
+        .send().await;
+    let res = match res {
+        Ok(res) => res,
+        Err(err) => {
+            eprintln!("{err:?}");
+            return
+        }
+    };
+    let json = res.json::<serde_json::Value>().await;
+    let json = match json {
+        Ok(val) => val,
+        Err(err) => {
+            eprintln!("{err:?}"),
+            return
+        }
+    };
     
+    let access_token = json.get("access_token");
+    let access_token: String = match access_token {
+        Some(token) => token,
+        None => {
+            eprintln!("access token not received");
+            return
+        }
+    };
+   
+    let user_info = client.get("https://www.googleapis.com/oauth2/v3/userinfo")
+        .bearer_auth(access_token.as_str())
+        .send()
+        .await;
+    
+    let user_info = match user_info {
+        Ok(info) => info,
+        Err(err) => {
+            eprintln!("{err:?}");
+            return
+        }
+    };
+    
+    let parsed_user_info = user_info.json::<serde_json::Value>().await;
+    let parsed_user_info = match parsed_user_info {
+        Ok(val) => val,
+        Err(err) => {
+            eprintln!("{err:?}");
+            return 
+        }
+    };
 } 
