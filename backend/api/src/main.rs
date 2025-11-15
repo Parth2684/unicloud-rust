4b1b3a96bcee4fbd829a8638f9ed18bba49df16e
Parth2684 <bhosle6006@gmail.com>
2025-11-15 21:02:15 +0530

    add cors

--- backend/api/src/main.rs
+++ backend/api/src/main.rs
@@ -1,6 +1,7 @@
-use axum::{Router, routing::get};
-
-use crate::routers::auth_router::auth_routes;
+use crate::{routers::auth_router::auth_routes, utils::export_envs::ENVS};
+use axum::{Router, http::HeaderValue, routing::get};
+use http::Method;
+use tower_http::cors::{Any, CorsLayer};
 
 mod handlers;
 mod routers;
@@ -8,9 +9,27 @@ mod utils;
 
 #[tokio::main]
 async fn main() {
+    let cors = CorsLayer::new()
+        .allow_origin(
+            ENVS.frontend_url
+                .to_string()
+                .parse::<HeaderValue>()
+                .unwrap(),
+        )
+        .allow_credentials(true)
+        .allow_headers(Any)
+        .allow_methods([
+            Method::GET,
+            Method::POST,
+            Method::PUT,
+            Method::DELETE,
+            Method::OPTIONS,
+        ]);
+
     let app = Router::<()>::new()
         .route("/", get(|| async { "Hello World" }))
-        .nest("/api/v1/auth", auth_routes());
+        .nest("/api/v1/auth", auth_routes())
+        .layer(cors);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
